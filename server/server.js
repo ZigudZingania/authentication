27ab44240fc6e4dad24a22ae6127255f413a6064
Rohan <rohangorla07@gmail.com>
2024-05-22 00:54:17 +0530

    connect to aws mysql database

--- server/server.js
+++ server/server.js
@@ -12,11 +12,11 @@ app.use(cors());
 const PORT = process.env.PORT;
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "r0hanr0n",
-  database: "serverone",
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
